3b35253d2121a5898437e8cd5fee190865272ea4
zahari00 <i@zaharystoyanov.com>
2021-09-22 20:47:57 +0300

    getAll data type catch

--- api/helpers/getAllData.ts
+++ api/helpers/getAllData.ts
@@ -1,6 +1,13 @@
-import { HomepageDTO } from '../dto';
-import fetch from 'isomorphic-unfetch'
+import { HomepageDTO } from "../dto";
+import fetch from "isomorphic-unfetch";
 
 export const getAllData = async (): Promise<HomepageDTO> => {
-  return await fetch(`${process.env.host}/api/homepage`).then((r) => r.json());
+  try {
+    return await fetch(`${process.env.host}/api/homepage`).then((r) =>
+      r.json()
+    );
+  } catch (e) {
+    // Sometimes the API is returning job and this results in error 
+    return await getAllData();
+  }
 };
