3d6cd787faef4be224523dbcfb8d5ad2c9449edf
zahari00 <i@zaharystoyanov.com>
2021-09-22 18:57:24 +0300

    optimaze webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.optimization = {
+      usedExports: true,
+      innerGraph: true,
+      sideEffects: true,
+    };
+    return config;
+  },
   env: {
     host: "http://localhost:3000",
     apiHost: "https://hacker-news.firebaseio.com/v0",
